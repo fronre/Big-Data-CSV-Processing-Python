1d459cf78784020a8845e14ad8e6a76586286d0d
fronre <islamhala35@gmail.com>
2026-03-01 19:48:21 +0100

    refactoring pandas_chunks.py

--- services/pandas_chunks.py
+++ services/pandas_chunks.py
@@ -1,9 +1,20 @@
 import pandas as pd
+import psutil
+import os
 from utils.timer import measure_performance
 
 @measure_performance
 def read_with_chunks(file_path, chunk_size=1000):
+    process = psutil.Process(os.getpid())
+    peak_memory = 0
     row_count = 0
+
     for chunk in pd.read_csv(file_path, chunksize=chunk_size):
         row_count += len(chunk)
-    return row_count
+
+        # نحسب الميموار في كل دورة
+        current_memory = process.memory_info().rss / (1024 ** 2)
+        peak_memory = max(peak_memory, current_memory)
+
+    # نرجع عدد الصفوف و الـ peak memory
+    return row_count, round(peak_memory, 2)

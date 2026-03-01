aabcfdd2b410dc9b51c781e68bbaebfb198055ff
fronre <islamhala35@gmail.com>
2026-03-01 19:47:40 +0100

    refactoring timer.py and add library psutil

--- utils/timer.py
+++ utils/timer.py
@@ -1,23 +1,21 @@
 import time
-from functools import wraps
-from memory_profiler import memory_usage
+import psutil
+import os
 
 def measure_performance(func):
-    @wraps(func)
     def wrapper(*args, **kwargs):
 
+        process = psutil.Process(os.getpid())
+        start_memory = process.memory_info().rss / (1024 ** 2)
+
         start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
 
-        mem_usage = memory_usage(
-            (func, args, kwargs),
-            interval=0.1,
-            retval=True
-        )
+        end_memory = process.memory_info().rss / (1024 ** 2)
 
-        memory_list, result = mem_usage
-
-        execution_time = round(time.time() - start_time, 2)
-        peak_memory = round(max(memory_list), 2)
+        execution_time = round(end_time - start_time, 2)
+        peak_memory = round(end_memory - start_memory, 2)
 
         return result, execution_time, peak_memory
 

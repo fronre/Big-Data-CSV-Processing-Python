901e14f519b6d0703b02a3101bf3140b4dfcb956
fronre <islamhala35@gmail.com>
2026-03-05 22:26:20 +0100

    refactoring

--- services/compressor.py
+++ services/compressor.py
@@ -4,24 +4,25 @@ import psutil
 import os
 from utils.timer import measure_performance
 
+
 @measure_performance
-def compress_file(input_path, output_path, chunk_size=100000):
+def compress_file(input_path, output_path):
 
     process = psutil.Process(os.getpid())
     peak_memory = 0
 
+    # قراءة الملف كامل
+    df = pd.read_csv(input_path)
+
+    # حساب الميموري بعد القراءة
+    current_memory = process.memory_info().rss / (1024 ** 2)
+    peak_memory = max(peak_memory, current_memory)
+
+    # كتابة الملف مضغوط
     with gzip.open(output_path, 'wt', newline='') as f_out:
-        for i, chunk in enumerate(pd.read_csv(input_path, chunksize=chunk_size)):
-
-            chunk.to_csv(
-
-                f_out,
-                header=(i == 0),
-                index=False
-            )
-
-            # نحسب الميموار في كل دورة
-            current_memory = process.memory_info().rss / (1024 ** 2)
-            peak_memory = max(peak_memory, current_memory)
+        df.to_csv(
+            f_out,
+            index=False
+        )
 
     return output_path, round(peak_memory, 2)
